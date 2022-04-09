ee1cd7ba41445a78c39465f8670b74d5dd986b46
EC2 Default User <ec2-user@ip-172-31-72-201.ec2.internal>
2022-04-09 02:37:17 +0000

    aws_provisioning: created s3_provisioning.py: Creates bucket, exports variable Issues: Does not export env vars to OS (need to work on Subprocess) to check if bucket already exists

---
+++ aws_provisioning/s3_provisioning.py
@@ -0,0 +1,31 @@
+import boto3
+import sys
+import os
+import subprocess
+from botocore.exceptions import ClientError
+
+s3 = boto3.client('s3')
+
+def s3CreateBuckets(region, BucketName):
+    CreateBucket = s3.create_bucket(Bucket=BucketName,CreateBucketConfiguration={
+        'LocationConstraint': region,
+    })
+    #response = s3.list_buckets()
+    
+    return CreateBucket
+    
+try:
+    if sys.argv != None and os.getenv('S3BUCKETURL') is None:
+        print(os.getenv('S3BUCKETURL'))
+        newbucket = s3CreateBuckets(sys.argv[1], sys.argv[2])
+        #print(newbucket['ResponseMetadata'])
+        if newbucket['ResponseMetadata']['HTTPStatusCode'] == 200:
+            print('Created Bucket '+ sys.argv[2]  + ' in region:', sys.argv[1])
+            subprocess.Popen(export S3BUCKETNAME=sys.argv[2])
+            os.putenv('S3BUCKETURL', str(newbucket['ResponseMetadata']['HTTPHeaders']['location']))
+            os.environ['S3BUCKETNAME'] = sys.argv[2]
+    else:
+        print('Bucket Already Exists')
+except ClientError as error:
+        print(error.response)
+    #print (newbuckets)
